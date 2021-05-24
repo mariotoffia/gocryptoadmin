7b09823beec297706858acb06589df9491b2e6f3
Mario Toffia <mario.toffia@gmail.com>
2021-05-24 08:55:03 +0200

    WIP Cost Unit transalation

---
+++ processors/costunit_test.go
@@ -0,0 +1,61 @@
+package processors
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/mariotoffia/gocryptoadmin/common"
+	"github.com/mariotoffia/gocryptoadmin/txlog"
+	"github.com/mariotoffia/gocryptoadmin/txlog/coinbasepro"
+	"github.com/mariotoffia/gocryptoadmin/utils"
+)
+
+func TestApplyEURCostUnit(t *testing.T) {
+
+	txr := txlog.NewTxLogReader(NewChronologicalTxEntryProcessor()).
+		RegisterReader("cb", coinbasepro.NewTransactionLogReader())
+
+	tx := txr.ReadBufferAsExchange(
+		"cb", utils.ReadFile("testfiles/cost-unit/cb.csv"),
+	)
+
+	proc := NewTxGroupProcessor(time.Hour * 20 /*20h*/)
+
+	for _, tx := range tx {
+		proc.Process(tx)
+	}
+
+	txg := proc.Flush()
+
+	acc := NewMultiExchangeAccountingProcessor()
+	for i := range txg {
+		acc.Process(&txg[i]) // Since accepting interface, use indexer
+	}
+
+	for exchange, transactions := range acc.Flush() {
+
+		if exchange != "cb" {
+			continue
+		}
+
+		cfa := transactions[0].(common.ConsoleFormatter)
+
+		fmt.Printf("\nExchange: %s\n\n", exchange)
+		fmt.Println(cfa.ConsoleHeader())
+
+		for i, tx := range transactions {
+
+			fmt.Println(
+				tx.(common.ConsoleFormatter).ConsoleString(),
+			)
+
+			if i == -1 {
+				break
+			}
+
+		}
+
+	}
+
+}
