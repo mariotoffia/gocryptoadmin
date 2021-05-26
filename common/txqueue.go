6ae92a7fefe55431e1ad83191a9654907182f939
Mario Toffia <mario.toffia@gmail.com>
2021-05-26 09:40:21 +0200

    added a asset tx FIFO queue

--- common/txqueue.go
+++ common/txqueue.go
@@ -2,8 +2,19 @@ package common
 
 import "github.com/mariotoffia/gocryptoadmin/utils"
 
+type FIFOTxQueue interface {
+	Enq(n TransactionEntry) FIFOTxQueue
+	Deq() TransactionEntry
+	IsEmpty() bool
+	Len() int
+}
+
 type TxFIFOQueue struct {
 	queue *utils.Queue
+}
+
+type TxAssetFIFOQueues struct {
+	queues map[AssetType]FIFOTxQueue
 }
 
 func NewTxFIFOQueue() *TxFIFOQueue {
@@ -15,7 +26,7 @@ func NewTxFIFOQueue() *TxFIFOQueue {
 }
 
 // Enq will enqueue the `TransactionLog`
-func (q *TxFIFOQueue) Enq(n *TransactionLog) *TxFIFOQueue {
+func (q *TxFIFOQueue) Enq(n TransactionEntry) FIFOTxQueue {
 
 	q.queue.PushBack(n)
 	return q
@@ -23,8 +34,8 @@ func (q *TxFIFOQueue) Enq(n *TransactionLog) *TxFIFOQueue {
 }
 
 // Deq will dequeue the first pushed `TransactionLog`.
-func (q *TxFIFOQueue) Deq() *TransactionLog {
-	return q.queue.PopFront().(*TransactionLog)
+func (q *TxFIFOQueue) Deq() TransactionEntry {
+	return q.queue.PopFront().(TransactionEntry)
 }
 
 func (q *TxFIFOQueue) IsEmpty() bool {
@@ -33,4 +44,45 @@ func (q *TxFIFOQueue) IsEmpty() bool {
 
 func (q *TxFIFOQueue) Len() int {
 	return q.queue.Len()
+}
+
+func NewTxAssetFIFOQueues() *TxAssetFIFOQueues {
+
+	return &TxAssetFIFOQueues{
+		queues: map[AssetType]FIFOTxQueue{},
+	}
+
+}
+
+// Enq will enqueue the `TransactionLog` into the _asset_ queue.
+func (q *TxAssetFIFOQueues) Enq(asset AssetType, n TransactionEntry) *TxAssetFIFOQueues {
+
+	q.getQueue(asset).Enq(n)
+	return q
+
+}
+
+// Deq will dequeue the first pushed `TransactionLog` onto the _asset_ queue.
+func (q *TxAssetFIFOQueues) Deq(asset AssetType) TransactionEntry {
+	return q.getQueue(asset).Deq().(TransactionEntry)
+}
+
+func (q *TxAssetFIFOQueues) IsEmpty(asset AssetType) bool {
+	return q.getQueue(asset).IsEmpty()
+}
+
+func (q *TxAssetFIFOQueues) Len(asset AssetType) int {
+	return q.getQueue(asset).Len()
+}
+
+func (q *TxAssetFIFOQueues) getQueue(asset AssetType) FIFOTxQueue {
+
+	queue := q.queues[asset]
+
+	if queue == nil {
+		queue = NewTxFIFOQueue()
+		q.queues[asset] = queue
+	}
+
+	return queue
 }
--- common/txqueue_test.go
+++ common/txqueue_test.go
@@ -16,8 +16,29 @@ func TestUseTxFIFOQueue(t *testing.T) {
 
 	assert.Equal(t, 3, q.Len())
 	assert.Equal(t, false, q.IsEmpty())
-	assert.Equal(t, "1", q.Deq().ID)
-	assert.Equal(t, "2", q.Deq().ID)
-	assert.Equal(t, "3", q.Deq().ID)
+	assert.Equal(t, "1", q.Deq().GetID())
+	assert.Equal(t, "2", q.Deq().GetID())
+	assert.Equal(t, "3", q.Deq().GetID())
 	assert.Equal(t, true, q.IsEmpty())
+}
+
+func TestUseTxAssetFIFOQueues(t *testing.T) {
+
+	q := NewTxAssetFIFOQueues()
+
+	q.Enq(AssetTypeBTC, &TransactionLog{ID: "1"}).
+		Enq(AssetTypeLTC, &TransactionLog{ID: "2"}).
+		Enq(AssetTypeBTC, &TransactionLog{ID: "3"})
+
+	assert.Equal(t, 2, q.Len(AssetTypeBTC))
+	assert.Equal(t, 1, q.Len(AssetTypeLTC))
+	assert.Equal(t, false, q.IsEmpty(AssetTypeLTC))
+	assert.Equal(t, false, q.IsEmpty(AssetTypeBTC))
+
+	assert.Equal(t, "2", q.Deq(AssetTypeLTC).GetID())
+	assert.Equal(t, "1", q.Deq(AssetTypeBTC).GetID())
+	assert.Equal(t, "3", q.Deq(AssetTypeBTC).GetID())
+
+	assert.Equal(t, true, q.IsEmpty(AssetTypeLTC))
+	assert.Equal(t, true, q.IsEmpty(AssetTypeBTC))
 }
