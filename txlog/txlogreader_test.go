1e2680932c8489a97793957a5babef71a8b2368c
Mario Toffia <mario.toffia@gmail.com>
2021-06-01 13:50:31 +0200

    added bitstamp txlog reader

--- txlog/txlogreader_test.go
+++ txlog/txlogreader_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/mariotoffia/gocryptoadmin/output"
 	"github.com/mariotoffia/gocryptoadmin/processors"
+	"github.com/mariotoffia/gocryptoadmin/txlog/bitstamp"
 	"github.com/mariotoffia/gocryptoadmin/txlog/bittrex"
 	"github.com/mariotoffia/gocryptoadmin/txlog/coinbasepro"
 	"github.com/mariotoffia/gocryptoadmin/txlog/kraken"
@@ -16,26 +17,6 @@ func TestReadCoinbasedTxLog(t *testing.T) {
 
 	tx := NewTxLogReader(processors.NewChronologicalTxEntryProcessor()).
 		UseDir("testfiles/cbx").
-		RegisterReader("cbx", coinbasepro.NewTransactionLogReader()).
-		Read()
-
-	proc := output.NewStdPrinterDefaults(os.Stdout, "default")
-
-	for _, tx := range tx {
-
-		proc.Process(&tx)
-	}
-
-	proc.Flush()
-
-	fmt.Println(len(tx))
-}
-
-func TestReadCoinbasedTxLogFiles(t *testing.T) {
-
-	tx := NewTxLogReader(processors.NewChronologicalTxEntryProcessor()).
-		UseDir("../data").
-		IgnoreUnknownFiles().
 		RegisterReader("cbx", coinbasepro.NewTransactionLogReader()).
 		Read()
 
@@ -75,6 +56,25 @@ func TestBittrexReadBuySell(t *testing.T) {
 	tx := NewTxLogReader(processors.NewChronologicalTxEntryProcessor()).
 		UseDir("testfiles/btx").
 		RegisterReader("btx", bittrex.NewTransactionLogReader()).
+		Read()
+
+	proc := output.NewStdPrinterDefaults(os.Stdout, "default")
+
+	for _, tx := range tx {
+
+		proc.Process(&tx)
+	}
+
+	proc.Flush()
+
+	fmt.Println(len(tx))
+}
+
+func TestBitsampBuySellReceiveTransfer(t *testing.T) {
+
+	tx := NewTxLogReader(processors.NewChronologicalTxEntryProcessor()).
+		UseDir("testfiles/bst").
+		RegisterReader("bst", bitstamp.NewTransactionLogReader()).
 		Read()
 
 	proc := output.NewStdPrinterDefaults(os.Stdout, "default")
